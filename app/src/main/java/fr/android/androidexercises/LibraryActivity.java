844e7abc0daae3ad81fb88b423ba215d550dc16e
gqueste <kakashi1hatake>
2016-01-21 15:56:00 +0100

    Layout

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -2,6 +2,9 @@ package fr.android.androidexercises;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -10,7 +13,12 @@ public class LibraryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
-        // TODO Get image view and load image form URL
+        ImageView imageView = (ImageView) findViewById(R.id.downloadImageView);
+
+        Picasso.with(getApplicationContext())
+                .load("http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg")
+                .into(imageView);
+        // Get image view and load image form URL
         // http://img3.wikia.nocookie.net/__cb20120317101541/harrypotter/images/3/37/Gryffindor_Crest.jpg
     }
 
