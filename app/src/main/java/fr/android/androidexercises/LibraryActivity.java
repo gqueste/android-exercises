495d8734e878c382b9480d6ab450728d1da9dfa7
gqueste <kakashi1hatake>
2016-01-21 13:57:18 +0100

    add default

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -33,8 +33,8 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
 
-        //  restore check box
-        checkBox.setChecked(savedInstanceState.getBoolean("checkbox"));
+        //  restore check box and add a default
+        checkBox.setChecked(savedInstanceState.getBoolean("checkbox", false));
     }
 
     @Override
