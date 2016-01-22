4bcd80aa8931c17088b2aa7bdfee788083247c75
gqueste <kakashi1hatake>
2016-01-22 11:25:56 +0100

    Appels REST

--- app/src/main/java/fr/android/androidexercises/HenriPotierService.java
+++ app/src/main/java/fr/android/androidexercises/HenriPotierService.java
@@ -1,7 +1,14 @@
 package fr.android.androidexercises;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.http.GET;
+
 public interface HenriPotierService {
 
-    // TODO Method GET books which return a List<Book>
+    // TODO Method GET books which return a List<Book
+    @GET("books")
+    Call<List<Book>> listBooks();
 
 }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,6 +3,13 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -15,17 +22,32 @@ public class LibraryActivity extends AppCompatActivity {
         // Plant logger cf. Android Timber
         Timber.plant(new Timber.DebugTree());
 
-        // TODO build Retrofit
+        // build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // TODO create a service
+        // create a service
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
 
-        // TODO listBooks()
+        // listBooks()
+        Call<List<Book>> call = service.listBooks();
+        call.enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Response<List<Book>> response, Retrofit retrofit) {
+                // success
+                for (Book b : response.body()) {
+                    Timber.i(b.getTitle());
+                }
+            }
 
-        // TODO enqueue call and display book title
-
-        // TODO log books
-
-        // TODO display book as a list
+            @Override
+            public void onFailure(Throwable t) {
+                // error occurred
+                Timber.e("Error on connexion");
+            }
+        });
     }
 
 }
