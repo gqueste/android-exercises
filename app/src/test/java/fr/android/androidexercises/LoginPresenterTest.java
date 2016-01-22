06dbacc19bcf0e2e2ab92ad726c6281f3f0a3600
gqueste <kakashi1hatake>
2016-01-22 11:58:02 +0100

    Unit tests

---
+++ app/src/test/java/fr/android/androidexercises/LoginPresenterTest.java
@@ -0,0 +1,26 @@
+package fr.android.androidexercises;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by Darknight on 22/01/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginPresenterTest{
+
+    @Mock LoginActivity activity;
+    @InjectMocks LoginPresenter loginPresenter;
+
+    @Test
+    public void testCheckCredentials() throws Exception {
+        loginPresenter.checkCredentials("password");
+        // Then
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+}
